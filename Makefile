590e7488e4ac73a7d1e5001194deb3f6d9595506
lavanya <lavanya@geekbull.in>
2017-04-21 17:02:38 +0530

    this is usng post method

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PROJECT = post
 PROJECT_DESCRIPTION = New project
 PROJECT_VERSION = 0.1.0
-DEPS = cowboy epgsql
+DEPS = cowboy epgsql 
 include erlang.mk
--- src/my_handler.erl
+++ src/my_handler.erl
@@ -1,35 +1,43 @@
 -module(my_handler).
 
--export([init/3]).
--export([content_types_accepted/2,allowed_methods/2]).
--export([to_html/2]).
+-export([init/3,terminate/3]).
+-export([resource_exists/2,allow_missing_post/2,content_types_accepted/2,allowed_methods/2]).
+-export([welcome/2]).
 
 init(_, _Req, _Opts) ->
 	{upgrade, protocol, cowboy_rest}.
+
+resource_exists(Req, State) ->
+  {false, Req, State}.
 allowed_methods(Req, State) ->
-	{[<<"GET">>, <<"POST">>], Req, State}.
+  {[<<"POST">>], Req, State}.
+
+allow_missing_post(Req, State) ->
+  {true, Req, State}.
+  
 content_types_accepted(Req, State) ->
-	{[{{<<"text">>, <<"html">>, '*'}, to_html}], Req, State}.
-to_html(Req, State) ->
-{ok, C} = epgsqla:start_link(),
+  {[{{<<"application">>, <<"x-www-form-urlencoded">>, '*'}, welcome}], Req, State}.
+
+terminate(_Reason, _Req, _State) ->
+  ok.
+welcome(Req, State) ->
+  {ok, C} = epgsqla:start_link(),
   Ref = epgsqla:connect(C, "localhost", "postgres", "postgres", [{database, "postgres"}]),
-    receive
-      {C, Ref, connected} ->
-        io:format("connected ~p\n", [C]);
-      {C, Ref, Error = {error, _}} ->
-        io:format("error is ~p", [Error])
-    end,
-{Query, Req1} = cowboy_req:qs(Req),
-  io:format(" query string value is : ~s\n", [Query]),
- {Mail, Req2} = cowboy_req:qs_val(<<"mailid">>, Req1),
-  io:format(" id is : ~s\n", [Mail]),
-  {Pass, Req3} = cowboy_req:qs_val(<<"password">>, Req2),
-  io:format(" password is : ~s\n", [Pass]),
-  SelectRes = epgsql:equery(C, "insert into details values($1,$2)", [Mail, Pass]),
-  io:format(" result is : ~p\n", [SelectRes]),
-  Result1 = epgsqla:close(C),
-  io:format("~s", [Result1]),
-  %%SelectRes1 = epgsql:equery(C, "insert into details values(0 ,'zero')"),
-  %%io:format(" result is : ~p\n", [SelectRes1]),
-  Body = <<"hi">>,
-	{Body,Req3, State}.
+  receive
+    {C, Ref, connected} ->
+      io:format("connected ~p", [C]);
+    {C, Ref, Error = {error, _}} ->
+          io:format("error is ~p", [Error])
+  end,
+  Req1 = cowboy_req:set_resp_header(<<"allow">>, "POST", Req),
+  Req2 = cowboy_req:set_resp_body(<<"HELLO POST">>, Req1),
+  {ok, KeyValues, Req3} = cowboy_req:body_qs(Req2),
+  {_, Id} = lists:keyfind(<<"mailid">>, 1, KeyValues),
+  io:format("mailid is ~s\n", [Id]),
+  {_, Pass} = lists:keyfind(<<"password">>, 1, KeyValues),
+  io:format("password is ~s\n", [Pass]),
+  SelectRes = epgsql:equery(C, "insert into details values($1,$2)", [Id, Pass]),
+  io:format("inserted ~p\n", [SelectRes]),
+  Close = epgsqla:close(C),
+  io:format("~s", [Close]),
+  {true, Req3, State}.
